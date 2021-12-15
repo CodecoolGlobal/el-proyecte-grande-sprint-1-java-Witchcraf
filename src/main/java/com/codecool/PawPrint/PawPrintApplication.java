11592b18a5622ffecf8dc80004b7c1b169434c67
Gyozo55 <74531373+Gyozo55@users.noreply.github.com>
2021-12-15 14:01:27 +0100

    start: init project

---
+++ src/main/java/com/codecool/PawPrint/PawPrintApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.PawPrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawPrintApplication.class, args);
+	}
+
+}

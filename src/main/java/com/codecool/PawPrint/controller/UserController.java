2bb0f64248691934a6d0e7b64a8e35f0e8d1dcea
Betti Bodnar <bettinabodnar7@gmail.com>
2021-12-15 15:53:13 +0100

    feat: add userController and userService

---
+++ src/main/java/com/codecool/PawPrint/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.codecool.PawPrint.controller;
+
+import com.codecool.PawPrint.model.User;
+import com.codecool.PawPrint.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path="/all", produces = "application/json")
+    public List<User> getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @GetMapping(path="/{id}", produces = "application/json")
+    public User getUserById(@PathVariable int id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping("/add")
+    public void registerNewUser( @RequestBody User user) {
+        userService.registerUser(user);
+    }
+
+}
---
+++ src/main/java/com/codecool/PawPrint/service/UserService.java
@@ -0,0 +1,31 @@
+package com.codecool.PawPrint.service;
+
+import com.codecool.PawPrint.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final List<User> users = new LinkedList<>();
+
+
+    public List<User> getAllUser() {
+        return users;
+    }
+
+    public void registerUser(User user) {
+        users.add(user);
+    }
+
+    public User findUserById(int id) {
+        User currentUser = null;
+        for (User user : users) {
+            if(user.getId() == id){
+                currentUser = user;
+            }
+        }
+        return currentUser;
+    }
+}

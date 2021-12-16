9999189bf6d7c77f82f5d758708f3a5d5bf9d684
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-12-16 19:17:57 +0100

    feat: add findServices() call to getServices() method in ServiceController class

--- src/main/java/com/codecool/PawPrint/controller/ServiceController.java
+++ src/main/java/com/codecool/PawPrint/controller/ServiceController.java
@@ -1,7 +1,8 @@
 package com.codecool.PawPrint.controller;
 
 import com.codecool.PawPrint.model.entity.PetType;
-import com.codecool.PawPrint.model.service.Service;
+import com.codecool.PawPrint.model.service.ServiceOffered;
+import com.codecool.PawPrint.model.service.ServiceSubtype;
 import com.codecool.PawPrint.model.service.ServiceType;
 import com.codecool.PawPrint.service.ServiceService;
 import com.codecool.PawPrint.service.UserService;
@@ -9,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -26,18 +26,18 @@ public class ServiceController {
         this.userService = userService;
     }
 
-    @GetMapping(value = "/search/{petType}/{country}/{city}/{district}/{service}/{serviceType}/{userId}")
-    public Set<Service> getServices(@PathVariable PetType petType, @PathVariable String country,
-                                     @PathVariable String city, @PathVariable String district,
-                                     @PathVariable String service, @PathVariable ServiceType serviceType,
-                                     @PathVariable Optional<Integer> userId) {
+    @GetMapping(value = "/search/{petType}/{country}/{city}/{district}/{serviceType}/{serviceSubtype}/{userId}")
+    public Set<ServiceOffered> getServices(@PathVariable PetType petType, @PathVariable String country,
+                                           @PathVariable String city, @PathVariable String district,
+                                           @PathVariable ServiceType serviceType, @PathVariable ServiceSubtype serviceSubtype,
+                                           @PathVariable Optional<Integer> userId) {
 
+        return serviceService.findServices(petType, country, city, district, serviceType, serviceSubtype);
 
-        return null;
     }
 
     @PostMapping(value = "/search/save/{userId}")
-    public String saveSearch(@PathVariable String userId, @RequestBody Set<Service> service) {
+    public String saveSearch(@PathVariable String userId, @RequestBody Set<ServiceOffered> service) {
         // userService.saveSearch(Integer.parseInt(userId), new Search(service));
         return "redirect:";
     }

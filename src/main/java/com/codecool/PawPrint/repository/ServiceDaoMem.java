b0fdc3d1db3010be07ab17644fbd39d95876b677
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-12-16 19:16:58 +0100

    feat: implement findServices() method in ServiceDaoMem class

--- src/main/java/com/codecool/PawPrint/repository/ServiceDaoMem.java
+++ src/main/java/com/codecool/PawPrint/repository/ServiceDaoMem.java
@@ -1,6 +1,9 @@
 package com.codecool.PawPrint.repository;
 
-import com.codecool.PawPrint.model.service.Service;
+import com.codecool.PawPrint.model.entity.PetType;
+import com.codecool.PawPrint.model.service.ServiceOffered;
+import com.codecool.PawPrint.model.service.ServiceSubtype;
+import com.codecool.PawPrint.model.service.ServiceType;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
@@ -9,26 +12,72 @@ import java.util.Set;
 @Component
 public class ServiceDaoMem implements ServiceDao {
 
-    private Set<Service> services = new HashSet<>();
+    private Set<ServiceOffered> services = new HashSet<>();
 
 
     @Override
-    public void add(Service service) {
+    public void add(ServiceOffered service) {
 
     }
 
     @Override
-    public Service findById(int id) {
+    public ServiceOffered findById(int id) {
         return null;
     }
 
     @Override
-    public Service findByName(String name) {
+    public ServiceOffered findByName(String name) {
         return null;
     }
 
     @Override
-    public Set<Service> getAll() {
+    public Set<ServiceOffered> findServices(PetType petType, String country, String city, String district, ServiceType serviceType, ServiceSubtype serviceSubtype) {
+        Set<ServiceOffered> foundServices = new HashSet<>();
+        Set<PetType> petTypeEquivalentSet = convertPetType(petType);
+
+        for (ServiceOffered serviceOffered : services) {
+            if (checkSearchCondition(serviceOffered, petTypeEquivalentSet, country, city,district, serviceType, serviceSubtype)) {
+                foundServices.add(serviceOffered);
+            }
+        }
+        return foundServices;
+    }
+
+    private boolean checkSearchCondition(ServiceOffered serviceOffered, Set<PetType> petTypeEquivalentSet, String country, String city, String district, ServiceType serviceType, ServiceSubtype serviceSubtype) {
+        return serviceOffered.getServiceType().equals(serviceType) && serviceOffered.getServiceSubtype().equals(serviceSubtype)
+                && petTypeEquivalentSet.contains(serviceOffered.getPetType()) && serviceOffered.getContact().getAddress().getCountry().equals(country)
+                && serviceOffered.getContact().getAddress().getCity().equals(city) && serviceOffered.getContact().getAddress().getDistrict().equals(district);
+    }
+
+    private Set<PetType> convertPetType(PetType petType) {
+        Set<PetType> petTypeEquivalentSet = new HashSet<>();
+        if (petType == null) {
+            petTypeEquivalentSet.add(PetType.CAT);
+            petTypeEquivalentSet.add(PetType.DOG);
+            petTypeEquivalentSet.add(PetType.CATANDDOG);
+        } else {
+            switch (petType) {
+                case CAT:
+                    petTypeEquivalentSet.add(PetType.CAT);
+                    petTypeEquivalentSet.add(PetType.CATANDDOG);
+                    break;
+                case DOG:
+                    petTypeEquivalentSet.add(PetType.DOG);
+                    petTypeEquivalentSet.add(PetType.CATANDDOG);
+                    break;
+                case CATANDDOG:
+                case NONE:
+                    petTypeEquivalentSet.add(PetType.CAT);
+                    petTypeEquivalentSet.add(PetType.DOG);
+                    petTypeEquivalentSet.add(PetType.CATANDDOG);
+                    break;
+            }
+        }
+        return petTypeEquivalentSet;
+    }
+
+    @Override
+    public Set<ServiceOffered> getAll() {
         return null;
     }
 }

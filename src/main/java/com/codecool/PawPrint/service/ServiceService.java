c053317d189af9a35349890d92953f37eca989df
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-12-16 19:14:25 +0100

    feat: add findServices() method to ServiceService class

--- src/main/java/com/codecool/PawPrint/service/ServiceService.java
+++ src/main/java/com/codecool/PawPrint/service/ServiceService.java
@@ -1,8 +1,14 @@
 package com.codecool.PawPrint.service;
 
+import com.codecool.PawPrint.model.entity.PetType;
+import com.codecool.PawPrint.model.service.ServiceSubtype;
+import com.codecool.PawPrint.model.service.ServiceOffered;
+import com.codecool.PawPrint.model.service.ServiceType;
 import com.codecool.PawPrint.repository.ServiceDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class ServiceService {
@@ -12,5 +18,10 @@ public class ServiceService {
     @Autowired
     public ServiceService(ServiceDao serviceDao) {
         this.serviceDao = serviceDao;
+    }
+
+    public Set<ServiceOffered> findServices(PetType petType, String country, String city, String district,
+                                           ServiceType serviceType, ServiceSubtype serviceSubtype) {
+        return serviceDao.findServices(petType, country, city, district, serviceType, serviceSubtype);
     }
 }

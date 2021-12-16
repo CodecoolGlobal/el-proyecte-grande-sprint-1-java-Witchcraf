e87752d3161b913aaa81fcd6e7cbb443987df46e
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-12-16 19:38:40 +0100

    feat: add saveSearch() method to UserService class

--- src/main/java/com/codecool/PawPrint/service/UserService.java
+++ src/main/java/com/codecool/PawPrint/service/UserService.java
@@ -1,10 +1,13 @@
 package com.codecool.PawPrint.service;
 
+import com.codecool.PawPrint.model.entity.Search;
 import com.codecool.PawPrint.model.entity.User;
+import com.codecool.PawPrint.model.service.ServiceOffered;
 import com.codecool.PawPrint.repository.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserService {
@@ -32,5 +35,10 @@ public class UserService {
        return userDao.findByName(name);
     }
 
+    public void saveSearch(int userId, Set<ServiceOffered> services) {
+        User user = findUserById(userId);
+        Search search = new Search(services);
+        userDao.add(user, search);
+    }
 
 }

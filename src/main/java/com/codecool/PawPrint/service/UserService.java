d423d18b37cd457f530aca87590dc52e46992c3f
Betti Bodnar <bettinabodnar7@gmail.com>
2021-12-16 13:17:41 +0100

    [#29] refactor: refact UserService logic.

--- src/main/java/com/codecool/PawPrint/service/UserService.java
+++ src/main/java/com/codecool/PawPrint/service/UserService.java
@@ -1,6 +1,8 @@
 package com.codecool.PawPrint.service;
 
 import com.codecool.PawPrint.model.entity.User;
+import com.codecool.PawPrint.repository.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
@@ -8,24 +10,23 @@ import java.util.List;
 
 @Service
 public class UserService {
-    private final List<User> users = new LinkedList<>();
 
+    private UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     public List<User> getAllUser() {
-        return users;
+        return userDao.getAll();
     }
 
     public void registerUser(User user) {
-        users.add(user);
+        userDao.add(user);
     }
 
     public User findUserById(int id) {
-        User currentUser = null;
-        for (User user : users) {
-            if(user.getId() == id){
-                currentUser = user;
-            }
-        }
-        return currentUser;
+       return userDao.findById(id);
     }
 }

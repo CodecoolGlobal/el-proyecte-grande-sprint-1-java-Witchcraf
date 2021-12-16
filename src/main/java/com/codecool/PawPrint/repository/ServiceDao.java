9def6bda74a1412af9ad69eef571f847e671f09f
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-12-16 19:16:42 +0100

    feat: add findServices() method to ServiceDao class

--- src/main/java/com/codecool/PawPrint/repository/ServiceDao.java
+++ src/main/java/com/codecool/PawPrint/repository/ServiceDao.java
@@ -1,13 +1,18 @@
 package com.codecool.PawPrint.repository;
 
-import com.codecool.PawPrint.model.service.Service;
+import com.codecool.PawPrint.model.entity.PetType;
+import com.codecool.PawPrint.model.service.ServiceOffered;
+import com.codecool.PawPrint.model.service.ServiceSubtype;
+import com.codecool.PawPrint.model.service.ServiceType;
 
 import java.util.Set;
 
 public interface ServiceDao {
 
-    void add(Service service);
-    Service findById(int id);
-    Service findByName(String name);
-    Set<Service> getAll();
+    void add(ServiceOffered service);
+    ServiceOffered findById(int id);
+    ServiceOffered findByName(String name);
+    Set<ServiceOffered> findServices(PetType petType, String country, String city, String district,
+                                     ServiceType service, ServiceSubtype serviceType);
+    Set<ServiceOffered> getAll();
 }

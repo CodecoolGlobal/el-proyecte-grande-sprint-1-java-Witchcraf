8e68922f4575dc36526d7fddf05d6478e74eed4c
Gyozo55 <74531373+Gyozo55@users.noreply.github.com>
2021-12-16 11:35:32 +0100

    [#35] feat: Create /add user route

--- src/main/java/com/codecool/PawPrint/controller/RegistrationController.java
+++ src/main/java/com/codecool/PawPrint/controller/RegistrationController.java
@@ -1,4 +1,27 @@
 package com.codecool.PawPrint.controller;
 
+
+import com.codecool.PawPrint.model.entity.User;
+import com.codecool.PawPrint.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reg")
 public class RegistrationController {
+
+    private final UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping("/add")
+    public void regNewUser(@RequestBody User user){
+        userService.registerUser(user);
+    }
+
+
 }

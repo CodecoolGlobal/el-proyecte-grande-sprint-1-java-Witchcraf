f29c57032fa4eef6f49ef1b3bcfd904ee8d4ade2
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-08 19:36:09 +0100

    feat: add Routes to App.js.

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import ServiceProviders from "./pages/serviceProviders";
 
 function App() {
-  return (
-    <div className="App">
-
-        <p>
-          PawPrint
-        </p>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/service-providers" element={<ServiceProviders />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;

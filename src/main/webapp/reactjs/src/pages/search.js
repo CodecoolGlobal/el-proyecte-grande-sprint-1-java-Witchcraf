65ef33aecfe7caa1b27c426a1e748e782ffd71cf
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-08 19:37:18 +0100

    feat: create basic pages implementations.

---
+++ src/main/webapp/reactjs/src/pages/search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from "./layout";
+
+function Search(){
+    return (
+        <Layout title="This is the search page">
+
+        </Layout>
+    );
+}
+export default Search;
---
+++ src/main/webapp/reactjs/src/pages/serviceProviders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from "./layout";
+
+function ServiceProviders(){
+    return (
+        <Layout title="This is the service page">
+
+        </Layout>
+    );
+}
+export default ServiceProviders;

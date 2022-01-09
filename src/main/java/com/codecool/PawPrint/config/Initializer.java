f7876b5b2cef193503a49d21b53c714a919cc110
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-09 09:46:34 +0100

    feat: add initilizer class to the application.

---
+++ src/main/java/com/codecool/PawPrint/config/Initializer.java
@@ -0,0 +1,31 @@
+package com.codecool.PawPrint.config;
+
+import com.codecool.PawPrint.model.entity.User;
+import com.codecool.PawPrint.model.entity.UserType;
+import com.codecool.PawPrint.repository.UserDao;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+
+@Component
+class Initializer implements CommandLineRunner {
+
+    private final UserDao repository;
+
+    public Initializer(UserDao repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) {
+        repository.add(new User("Pete", LocalDateTime.now(), "pete@gmail.com", "1234", UserType.NORMAL));
+        repository.add(new User("admin", LocalDateTime.now(), "admin", "admin", UserType.ADMIN));
+        repository.add(new User("Eve", LocalDateTime.now(), "eve@gmail.com", "1234", UserType.NORMAL));
+        repository.add(new User("John", LocalDateTime.now(), "john@gmail.com", "1234", UserType.NORMAL));
+        repository.add(new User("Rob", LocalDateTime.now(), "rob@gmail.com", "1234", UserType.NORMAL));
+        User first = repository.findByName("Pete");
+        System.out.println(first.getEmail());
+    }
+}

5534de8cb1183dae1263d05782d3640655d894e4
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-12 23:39:36 +0100

    feat: create converter to handle string converter to enum.

---
+++ src/main/java/com/codecool/PawPrint/util/JsonResponseConverter.java
@@ -0,0 +1,36 @@
+package com.codecool.PawPrint.util;
+
+import com.codecool.PawPrint.model.entity.PetType;
+import com.codecool.PawPrint.model.service.ServiceType;
+
+public class JsonResponseConverter {
+
+    public static PetType getPetTypeFromJsonString(String petType){
+        if(petType.equalsIgnoreCase("cat")){
+            return PetType.CAT;
+        }
+        if(petType.equalsIgnoreCase("dog")){
+            return PetType.DOG;
+        }
+        if(petType.equalsIgnoreCase("Cat&Dog")){
+            return PetType.CATANDDOG;
+        }
+        return PetType.NONE;
+    }
+
+    public static ServiceType getServiceTyoeFromJsonString(String serviceType) {
+        if(serviceType.equalsIgnoreCase("restaurant")){
+            return ServiceType.RESTAURANT;
+        }
+        if(serviceType.equalsIgnoreCase("wellness")){
+            return ServiceType.WELLNESS;
+        }
+        if(serviceType.equalsIgnoreCase("shelter")){
+            return ServiceType.SHELTER;
+        }
+        if(serviceType.equalsIgnoreCase("hospital")){
+            return ServiceType.HOSPITAL;
+        }
+        return null;
+    }
+}

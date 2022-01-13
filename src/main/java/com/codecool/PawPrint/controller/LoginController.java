48bc82ffaf49f8c49d6ad8785ebda080ed0b22d8
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-13 21:49:44 +0100

    style: delete all unused code.

--- src/main/java/com/codecool/PawPrint/controller/LoginController.java
+++ src/main/java/com/codecool/PawPrint/controller/LoginController.java
@@ -7,8 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
-//    @GetMapping("/login")
-//    public String login(){
-//        return "hello";
-//    }
 }
--- src/main/java/com/codecool/PawPrint/controller/ServiceController.java
+++ src/main/java/com/codecool/PawPrint/controller/ServiceController.java
@@ -7,11 +7,8 @@ import com.codecool.PawPrint.model.service.ServiceSubtype;
 import com.codecool.PawPrint.model.service.ServiceType;
 import com.codecool.PawPrint.service.ServiceService;
 import com.codecool.PawPrint.service.UserService;
-import com.codecool.PawPrint.util.JsonResponseConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 import java.util.Set;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -28,45 +25,20 @@ public class ServiceController {
         this.userService = userService;
     }
 
-    @PostMapping(value="/searchTest", produces = "application/json")
-    @ResponseBody
-    public Set<ServiceOffered> getServicesBy3field(@RequestBody Map<String, String> payload) {
-        PetType petType = JsonResponseConverter.getPetTypeFromJsonString(payload.get("petType"));
-        ServiceType serviceType = JsonResponseConverter.getServiceTypeFromJsonString(payload.get("serviceType"));
-        String country = payload.get("country");
-        String region = payload.get("region");
-        String district = payload.get("district");
-
-        System.out.println(payload);
-        Set<ServiceOffered> result = serviceService.findServices(petType, country, region, district, serviceType);
-        System.out.println(result);
-        return result;
-    }
-
-
     @PostMapping(value = "/search")
     public Set<ServiceOffered> getServices(@RequestBody SearchService searchService) {
-
         PetType petType = searchService.getPetType();
         String country = searchService.getCountry();
         String city = searchService.getCity();
         String district = searchService.getDistrict();
         ServiceType serviceType = searchService.getServiceType();
         ServiceSubtype serviceSubtype = searchService.getServiceSubtype();
-        System.out.println(petType);
-        System.out.println(country);
-        System.out.println(city);
-        System.out.println(district);
-        System.out.println(serviceType);
-        System.out.println(serviceSubtype);
 
         if (serviceSubtype != null) {
             return serviceService.findServices(petType, country, city, district, serviceType, serviceSubtype);
         }
         return serviceService.findServices(petType, country, city, district, serviceType);
-
     }
-
 
     @PostMapping(value = "/search/save")
     @ResponseBody

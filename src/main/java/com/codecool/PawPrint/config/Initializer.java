3e9110bdbb3b4bb3dac23bfc388db6f618ed2ff4
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2022-01-26 12:24:08 +0100

    refact: comment out test users with their contacts and services

--- src/main/java/com/codecool/PawPrint/config/Initializer.java
+++ src/main/java/com/codecool/PawPrint/config/Initializer.java
@@ -11,6 +11,7 @@ import com.codecool.PawPrint.model.service.ServiceType;
 import com.codecool.PawPrint.repository.ServiceDao;
 import com.codecool.PawPrint.repository.UserDao;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,7 @@ class Initializer implements CommandLineRunner {
     private final ServiceDao dao;
     private final PasswordEncoder bCryptPasswordEncoder;
 
-    public Initializer(UserDao repository, ServiceDao dao, PasswordEncoder bCryptPasswordEncoder) {
+    public Initializer(UserDao repository, @Qualifier("serviceDaoJPA") ServiceDao dao, PasswordEncoder bCryptPasswordEncoder) {
         this.repository = repository;
         this.dao = dao;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
@@ -35,36 +36,36 @@ class Initializer implements CommandLineRunner {
     public void run(String... strings) {
 
 
-        User pete = new User("Zoe", LocalDateTime.now(), "cafezoo@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        pete.setId(1);
-        User admin = new User("admin", LocalDateTime.now(), "admin", bCryptPasswordEncoder.encode("admin"), UserType.ADMIN);
-        admin.setId(0);
-        User eve = new User("Eve", LocalDateTime.now(), "eve@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        eve.setId(2);
-        User John = new User("John", LocalDateTime.now(), "john@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        John.setId(3);
-        User rob = new User("Rob", LocalDateTime.now(), "rob@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        rob.setId(4);
+//        User pete = new User("Zoe", LocalDateTime.now(), "cafezoo@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
+//        pete.setId(1);
+//        User admin = new User("admin", LocalDateTime.now(), "admin", bCryptPasswordEncoder.encode("admin"), UserType.ADMIN);
+//        admin.setId(0);
+//        User eve = new User("Eve", LocalDateTime.now(), "eve@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
+//        eve.setId(2);
+//        User John = new User("John", LocalDateTime.now(), "john@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
+//        John.setId(3);
+//        User rob = new User("Rob", LocalDateTime.now(), "rob@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
+//        rob.setId(4);
         User washAndVauUser = new User("WashAndVau", LocalDateTime.now(), "info@kutyamoso.hu", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        washAndVauUser.setId(5);
+//        washAndVauUser.setId(5);
         User washAndVauUserKecskemet = new User("WashAndVauKecskemét", LocalDateTime.now(), "kecskemet@kutyamoso.hu", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        washAndVauUser.setId(6);
+//        washAndVauUser.setId(6);
         User BBsUser = new User("BBsBar&Grill", LocalDateTime.now(), "hello@bbzbar.hu", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        BBsUser.setId(7);
+//        BBsUser.setId(7);
         User HokedliUser = new User("Hokedli", LocalDateTime.now(), "hokedlidelivery@gmail.com", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        HokedliUser.setId(8);
+//        HokedliUser.setId(8);
         User WanHaoUser = new User("WanHao", LocalDateTime.now(), "info@monoricenter.hu", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        WanHaoUser.setId(9);
+//        WanHaoUser.setId(9);
         User TerezaUser = new User("Tereza", LocalDateTime.now(), "RESERVATION@TEREZA.HU", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        WanHaoUser.setId(10);
+//        WanHaoUser.setId(10);
         User NaspolyaUser = new User("Naspolya", LocalDateTime.now(), "info@naspolya.hu", bCryptPasswordEncoder.encode("1234"), UserType.NORMAL);
-        WanHaoUser.setId(10);
+//        WanHaoUser.setId(10);
 
-        repository.add(pete);
-        repository.add(admin);
-        repository.add(eve);
-        repository.add(John);
-        repository.add(rob);
+//        repository.add(pete);
+//        repository.add(admin);
+//        repository.add(eve);
+//        repository.add(John);
+//        repository.add(rob);
         repository.add(washAndVauUser);
         repository.add(washAndVauUserKecskemet);
         repository.add(BBsUser);
@@ -75,11 +76,11 @@ class Initializer implements CommandLineRunner {
 
 
         //init contact
-        Contact petecon = new Contact(pete);
-        Contact admincon = new Contact(admin);
-        Contact evecon = new Contact(eve);
-        Contact johncon = new Contact(John);
-        Contact robCon= new Contact(rob);
+//        Contact petecon = new Contact(pete);
+//        Contact admincon = new Contact(admin);
+//        Contact evecon = new Contact(eve);
+//        Contact johncon = new Contact(John);
+//        Contact robCon= new Contact(rob);
         Contact washAndVauCon= new Contact(washAndVauUser, "+36205450145");
         Contact washAndVau2Con= new Contact(washAndVauUser, "+36205450145");
         Contact washAndVau3Con= new Contact(washAndVauUser, "+36205450145");
@@ -93,11 +94,11 @@ class Initializer implements CommandLineRunner {
 
 
         //init address
-        Address peteAddress = new Address(petecon.getId(), "Hungary", "Budapest", "6");
-        Address adminAdd = new Address(admincon.getId(), "Hungary", "Budapest", "6");
-        Address eveAddress = new Address(evecon.getId(), "Hungary", "Budapest", "5");
-        Address johnAddress = new Address(evecon.getId(), "Hungary", "Kecskemét", "5");
-        Address robAddress = new Address(evecon.getId(), "Hungary", "Kecskemét", "5");
+//        Address peteAddress = new Address(petecon.getId(), "Hungary", "Budapest", "6");
+//        Address adminAdd = new Address(admincon.getId(), "Hungary", "Budapest", "6");
+//        Address eveAddress = new Address(evecon.getId(), "Hungary", "Budapest", "5");
+//        Address johnAddress = new Address(evecon.getId(), "Hungary", "Kecskemét", "5");
+//        Address robAddress = new Address(evecon.getId(), "Hungary", "Kecskemét", "5");
         Address washAndVauAddress = new Address(washAndVauCon.getId(), "Hungary", "11", "Budapest", "Tétényi út", "31");
         Address washAndVau2Address = new Address(washAndVau2Con.getId(), "Hungary", "3", "Budapest", "Margitliget utca", "1");
         Address washAndVau3Address = new Address(washAndVau3Con.getId(), "Hungary", "3", "Budapest", "Bécsi út", "67");
@@ -110,11 +111,11 @@ class Initializer implements CommandLineRunner {
         Address NaspolyaAddress = new Address(NaspolyaCon.getId(), "Hungary", "6", "Budapest", "Káldy Gyula utca", "7");
 
         //set address to contact
-        petecon.setAddress(peteAddress);
-        admincon.setAddress(adminAdd);
-        evecon.setAddress(eveAddress);
-        johncon.setAddress(johnAddress);
-        robCon.setAddress(robAddress);
+//        petecon.setAddress(peteAddress);
+//        admincon.setAddress(adminAdd);
+//        evecon.setAddress(eveAddress);
+//        johncon.setAddress(johnAddress);
+//        robCon.setAddress(robAddress);
         washAndVauCon.setAddress(washAndVauAddress);
         washAndVau2Con.setAddress(washAndVau2Address);
         washAndVau3Con.setAddress(washAndVau3Address);
@@ -127,15 +128,15 @@ class Initializer implements CommandLineRunner {
         NaspolyaCon.setAddress(NaspolyaAddress);
 
         // init ServiceOffered
-        ServiceOffered cosP = new ServiceOffered("WashAndWau", PetType.CAT, ServiceType.WELLNESS, ServiceSubtype.WASHANDVAU, petecon);
-        ServiceOffered animalHotel = new ServiceOffered("Pangea", PetType.CATANDDOG, ServiceType.WELLNESS, ServiceSubtype.COSMETICS, johncon);
-        ServiceOffered restP = new ServiceOffered("Café Zoo", PetType.CAT, ServiceType.RESTAURANT, null, petecon);
-        ServiceOffered restP2 = new ServiceOffered("Zoo", PetType.CAT, ServiceType.RESTAURANT, null, petecon);
-        ServiceOffered hospP = new ServiceOffered("Profivet", PetType.DOG, ServiceType.HEALTHCARE, ServiceSubtype.HOSPITAL, admincon);
-        ServiceOffered sheltE = new ServiceOffered("Vizsla Bárka", PetType.CATANDDOG, ServiceType.SHELTER, null, evecon);
-        ServiceOffered sheltFutr = new ServiceOffered("Futrinka", PetType.CATANDDOG, ServiceType.SHELTER, null, robCon);
-        ServiceOffered restE = new ServiceOffered("Fellini Bisztró", PetType.CATANDDOG, ServiceType.RESTAURANT, null, evecon);
-        ServiceOffered garden = new ServiceOffered("Kertem", PetType.DOG, ServiceType.RESTAURANT, null, johncon);
+//        ServiceOffered cosP = new ServiceOffered("WashAndWau", PetType.CAT, ServiceType.WELLNESS, ServiceSubtype.WASHANDVAU, petecon);
+//        ServiceOffered animalHotel = new ServiceOffered("Pangea", PetType.CATANDDOG, ServiceType.WELLNESS, ServiceSubtype.COSMETICS, johncon);
+//        ServiceOffered restP = new ServiceOffered("Café Zoo", PetType.CAT, ServiceType.RESTAURANT, null, petecon);
+//        ServiceOffered restP2 = new ServiceOffered("Zoo", PetType.CAT, ServiceType.RESTAURANT, null, petecon);
+//        ServiceOffered hospP = new ServiceOffered("Profivet", PetType.DOG, ServiceType.HEALTHCARE, ServiceSubtype.HOSPITAL, admincon);
+//        ServiceOffered sheltE = new ServiceOffered("Vizsla Bárka", PetType.CATANDDOG, ServiceType.SHELTER, null, evecon);
+//        ServiceOffered sheltFutr = new ServiceOffered("Futrinka", PetType.CATANDDOG, ServiceType.SHELTER, null, robCon);
+//        ServiceOffered restE = new ServiceOffered("Fellini Bisztró", PetType.CATANDDOG, ServiceType.RESTAURANT, null, evecon);
+//        ServiceOffered garden = new ServiceOffered("Kertem", PetType.DOG, ServiceType.RESTAURANT, null, johncon);
         ServiceOffered washAndVau = new ServiceOffered("Wash & Vau Újbuda", PetType.DOG, 4.4, washAndVauCon, "Open 24 hours", ServiceType.WELLNESS, ServiceSubtype.WASHANDVAU, "Önkiszolgáló kutyafürdetés gyorsan és egyszerűen kutyamosónk segítségével a nap 24 órájában! :)", "https://kutyamoso.hu/ujbuda/");
         ServiceOffered washAndVau2 = new ServiceOffered("Wash & Vau Békásmegyer", PetType.DOG, 4.4, washAndVau2Con, "Open 24 hours", ServiceType.WELLNESS, ServiceSubtype.WASHANDVAU, "Önkiszolgáló kutyafürdetés gyorsan és egyszerűen kutyamosónk segítségével a nap 24 órájában! :)", "https://kutyamoso.hu/bekasmegyer/");
         ServiceOffered washAndVau3 = new ServiceOffered("Wash & Vau Óbuda", PetType.DOG, 4.4, washAndVau3Con, "Open 24 hours", ServiceType.WELLNESS, ServiceSubtype.WASHANDVAU, "Önkiszolgáló kutyafürdetés gyorsan és egyszerűen kutyamosónk segítségével a nap 24 órájában! :)", "https://kutyamoso.hu/obuda/");
@@ -147,15 +148,15 @@ class Initializer implements CommandLineRunner {
         ServiceOffered Tereza = new ServiceOffered("Tereza Mexican Restaurant", PetType.DOG, 4.3, TerezaCon, "H-P 18:00-00:00 Szo-V: 12:00-00:00", ServiceType.RESTAURANT, null, "MI CASA ES SU CASA! MEXICAN URBAN GARDEN IN DOWNTOWN BUDAPEST. TEREZA HAS TAKEN HER PERMANENT PLACE AND AWAITS ITS GUESTS WITH UNIQUE AMBIENCE, AUTHENTIC MEXICAN CUISINE BY REGIONS AND WILD SELECTION OF MEZCALS AND MARGARITAS.", "https://tereza.hu/");
         ServiceOffered Naspolya = new ServiceOffered("Naspolya Nassolda", PetType.DOG, 4.5, NaspolyaCon, "K-P 12:00-19:00 Szo-V 10:00-19:00", ServiceType.RESTAURANT, null, "Naspolya is a pioneering downtown café where all the sweets and salty snacks are raw vegan, made from organic fair trade ingredients, without additives or empty calories.", "https://naspolya.hu");
 
-        dao.add(cosP);
-        dao.add(restP);
-        dao.add(hospP);
-        dao.add(sheltE);
-        dao.add(restE);
-        dao.add(restP2);
-        dao.add(garden);
-        dao.add(animalHotel);
-        dao.add(sheltFutr);
+//        dao.add(cosP);
+//        dao.add(restP);
+//        dao.add(hospP);
+//        dao.add(sheltE);
+//        dao.add(restE);
+//        dao.add(restP2);
+//        dao.add(garden);
+//        dao.add(animalHotel);
+//        dao.add(sheltFutr);
         dao.add(washAndVau);
         dao.add(washAndVau2);
         dao.add(washAndVau3);

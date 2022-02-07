02589515e24614e9efaa7ac2a9d358e2167b2060
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2022-02-07 19:53:48 +0100

    feat: implement search results saving in searchResult component

--- src/main/webapp/reactjs/src/component/searchResult.js
+++ src/main/webapp/reactjs/src/component/searchResult.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import ResultCard from "../component/resultCard";
+import {Form} from "react-bootstrap";
+
 
 
 function SearchResult({results}){
+
+    const [searches, setSearches] = useState({
+        id: null,
+        name: "My Search",
+        searchedServices: []
+    })
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // TODO: modal for naming search
+        await saveSearchResults(searches);
+    }
+
+    const fetchSaveSearch = async (searches) => {
+        const res = await fetch(`/api/search/save`,{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(searches)  // user identification?
+        })
+        setSearches({       // refact?
+            id: null,
+            name: "My Search",
+            searchedServices: []
+        })
+        return await res.json();
+    }
+
+    const saveSearchResults = async (search) => {
+        await fetchSaveSearch(search);
+    }
+
     return (
+        <Form onSubmit={handleSubmit}>
             <div className='results'>
                 {results.map((result, index) => (
-                    <ResultCard key={index} result={result} />
+                    <ResultCard key={index} result={result} searches={setSearches}/>
                 ))}
+                {
+                    searches.searchedServices.length !== 0 ?
+                        <multiButton label={"Save Search"} bWidth={"100%"}/> : null
+                }
             </div>
+        </Form>
     );
 
 }

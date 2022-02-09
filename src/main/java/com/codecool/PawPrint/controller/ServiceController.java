948346df27081f004c7ef681c0bf270f486d9fdb
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2022-02-09 17:50:21 +0100

    feat: add search saving to ServiceController class

--- src/main/java/com/codecool/PawPrint/controller/ServiceController.java
+++ src/main/java/com/codecool/PawPrint/controller/ServiceController.java
@@ -2,6 +2,8 @@ package com.codecool.PawPrint.controller;
 
 import com.codecool.PawPrint.model.controllerEntity.SaveSearchEntity;
 import com.codecool.PawPrint.model.controllerEntity.SearchService;
+import com.codecool.PawPrint.model.entity.Search;
+import com.codecool.PawPrint.model.entity.User;
 import com.codecool.PawPrint.model.service.ServiceOffered;
 import com.codecool.PawPrint.model.service.ServiceSubtype;
 import com.codecool.PawPrint.model.service.ServiceType;
@@ -55,9 +57,11 @@ public class ServiceController {
 
     @PostMapping(value = "/search/save")
     @ResponseBody
-    public SaveSearchEntity saveSearch(@RequestBody SaveSearchEntity saveSearchEntity) {
+    public Search saveSearch(@RequestBody SaveSearchEntity saveSearchEntity) {
         System.out.println(saveSearchEntity);
-//        userService.saveSearch(Integer.parseInt(userId), services);
-        return saveSearchEntity;
+        User user = userService.findUserByName(saveSearchEntity.getUsername());
+        Set<ServiceOffered> services = new HashSet<>();
+        saveSearchEntity.getSearchedServices().forEach(serviceId -> services.add(serviceService.findServiceById(serviceId)));
+        return userService.saveSearch(user,services, saveSearchEntity.getSearchName(), saveSearchEntity.getDescription());
     }
 }

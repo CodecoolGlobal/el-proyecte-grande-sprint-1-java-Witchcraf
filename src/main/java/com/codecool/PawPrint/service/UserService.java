69d31e6346b8a7fcd8181933a87538c6e7f2e038
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2022-02-09 17:48:42 +0100

    refact: lowercase findByUserName to findByUsername in user repository classes

--- src/main/java/com/codecool/PawPrint/service/UserService.java
+++ src/main/java/com/codecool/PawPrint/service/UserService.java
@@ -1,7 +1,12 @@
 package com.codecool.PawPrint.service;
 
+import com.codecool.PawPrint.model.controllerEntity.SaveSearchEntity;
+import com.codecool.PawPrint.model.entity.Search;
 import com.codecool.PawPrint.model.entity.User;
+import com.codecool.PawPrint.model.service.ServiceOffered;
+import com.codecool.PawPrint.repository.SearchDao;
 import com.codecool.PawPrint.repository.UserDao;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,21 +15,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final UserDao userDao;
+    private final SearchDao searchDao;
     private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public UserService(UserDao userDao, PasswordEncoder passwordEncoder) {
-        this.userDao = userDao;
-        this.passwordEncoder = passwordEncoder;
-    }
+//    @Autowired
+//    public UserService(UserDao userDao, PasswordEncoder passwordEncoder) {
+//        this.userDao = userDao;
+//        this.passwordEncoder = passwordEncoder;
+//    }
 
     public List<User> getAllUser() {
         return userDao.getAll();
@@ -41,7 +46,7 @@ public class UserService implements UserDetailsService {
     }
 
     public User findUserByName(String name) {
-       return userDao.findByUserName(name);
+       return userDao.findByUsername(name);
     }
 
     public boolean checkRegEmailAndName(String email, String fullName){
@@ -51,15 +56,17 @@ public class UserService implements UserDetailsService {
         return true;
     }
 
-//    public void saveSearch(int userId, Set<ServiceOffered> services) {
-//        User user = findUserById(userId);
-//        Search search = new Search(services);
-//        userDao.add(user, search);
-//    }
+    public Search saveSearch(User user, Set<ServiceOffered> servicesInSearch, String searchName, String description) {
+        Search search = new Search(0, searchName, description, user, servicesInSearch);
+        user.getSavedSearches().add(search);
+        userDao.add(user);
+        searchDao.add(search);
+        return search;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userDao.findByUserName(username);
+        User user = userDao.findByUsername(username);
         Collection<SimpleGrantedAuthority> userTypes = new ArrayList<>();
         if(user == null){
             throw new UsernameNotFoundException("Not found this User!");

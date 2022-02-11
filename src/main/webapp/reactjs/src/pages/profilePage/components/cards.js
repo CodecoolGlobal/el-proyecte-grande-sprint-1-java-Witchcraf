9265e82a43102c0021ea412e455579db95c1934e
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2022-02-11 01:39:52 +0100

    feat: add search title and description to cards

--- src/main/webapp/reactjs/src/pages/profilePage/components/cards.js
+++ src/main/webapp/reactjs/src/pages/profilePage/components/cards.js
@@ -7,11 +7,10 @@ function Cards({user, details}){
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
-                {user.role === "ADMIN" ? <Card.Title>Service Title</Card.Title> : <Card.Title>Search Title</Card.Title>}
+                {user.role === "ADMIN" ? <Card.Title>Service Title</Card.Title> : <Card.Title>{details.name}</Card.Title>}
                 {user.role === "ADMIN" ? <Card.Subtitle className="mb-2 text-muted">Description</Card.Subtitle> : <Card.Subtitle className="mb-2 text-muted">Description</Card.Subtitle>}
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    {details.description}
                 </Card.Text>
                 <ButtonGroup aria-label="Basic example">
                     <Button variant="danger">Delete</Button>
